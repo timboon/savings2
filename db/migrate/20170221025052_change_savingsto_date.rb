90e683c96ee3c6023e8cb56ab47d85b070a95689
Oliver <blind118@hotmail.com>
2017-02-25 07:51:38 +0000

    changed migration

--- db/migrate/20170221025052_change_savingsto_date.rb
+++ db/migrate/20170221025052_change_savingsto_date.rb
@@ -1,6 +1,7 @@
 class ChangeSavingstoDate < ActiveRecord::Migration[5.0]
   def change
     change_column :savings, :week, :date
+    change_column :savings, :week, 'date USING CAST(week AS date)'
   end
 end
     
